56012e846019e79ac6809c72fb469a2b7fcf7fa9
tloftis <tloftis@redviking.com>
2015-11-27 14:50:12 -0500

    Cleaned up lots of cod :memo: :art: Bettered the server side handling of clients :art: Added repetitive code to functions instead of it being repetitive

--- client/modules/plane-game/controllers/plane-game-multi.client.controller.js
+++ client/modules/plane-game/controllers/plane-game-multi.client.controller.js
@@ -6,19 +6,19 @@
 // Reports controller
 angular.module('plane-game').controller('planeGameMultiController', ['$rootScope', '$scope', '$state', 'Alerts', '$location',
     function ($rootScope, $scope, $state, Alerts, $location) {
-        console.log('Multi player');
+        console.log('Multi-player');
 
         $scope.playerMv = {};
         $scope.players = [];
         $scope.fMissles = [];
         $scope.enemies = [];
         $scope.eMissles = [];
+        $scope.socket = $rootScope.socket;
 
         var field = angular.element(document.getElementsByName('planeparent'));
         var playerBaseClass = 'fa fa-plane fa-rotate-45 ';
 
-        $scope.socket = $rootScope.socket;
-
+        //Allow chat though the console
         window.chat = function(data){
             $scope.socket.emit('chat', data);
         };
@@ -26,69 +26,101 @@ angular.module('plane-game').controller('planeGameMultiController', ['$rootScope
         window.chatSetName = function(data){
             $scope.socket.emit('chat-setname', data);
         };
+        
+        $scope.init = function(){
+            $scope.player = createPlayer();
+            $scope.player.className += ' text-success';
+        };
 
         $scope.socket.on('you-dead', function(data) {
             console.log('You\'ve been un-alived');
+
+            $scope.player.parentNode.removeChild($scope.fMissles[0]);
+            $scope.player = {};
         });
 
         $scope.socket.on('multiplier-update', function(data) {
-            data.players[data.id].class = 'text-success';
-            data.players = Object.keys(data.players).map(function(key){ return( data.players[key] ); });
-
             //remove dead players, enemies, bullets
-            while(data.eMissles.length < $scope.eMissles.length){
-                $scope.eMissles[0].parentNode.removeChild($scope.eMissles[0]);
-                $scope.eMissles.shift();
-            }
-            while(data.fMissles.length < $scope.fMissles.length){
-                $scope.fMissles[0].parentNode.removeChild($scope.fMissles[0]);
-                $scope.fMissles.shift();
-            }
-            while(data.enemies.length < $scope.enemies.length){
-                $scope.enemies[0].parentNode.removeChild($scope.enemies[0]);
-                $scope.enemies.shift();
-            }
-            while(data.players.length < $scope.players.length){
-                $scope.players[0].parentNode.removeChild($scope.players[0]);
-                $scope.players.shift();
-            }
+            while(data.eMissles.length < $scope.eMissles.length)
+                removeEMissles(eMissles[0], 0);
+
+            while(data.fMissles.length < $scope.fMissles.length)
+                removeFMissle(fMissles[0], 0);
+
+            while(data.enemies.length < $scope.enemies.length)
+                removeEnemy(enemies[0], 0);
+
+            while(data.players.length < $scope.players.length)
+                removePlayer(players[0], 0);
 
             //add new enemies, bullets, players
-            while(data.players.length > $scope.players.length){
-                $scope.createPlayer();
-            }
-            while(data.enemies.length > $scope.enemies.length){
-                $scope.createEnemy();
-            }
-            while(data.fMissles.length > $scope.fMissles.length){
-                $scope.createBody();
-            }
-            while(data.eMissles.length > $scope.eMissles.length){
-                $scope.createEBody();
-            }
+            while(data.players.length > $scope.players.length)
+                $scope.players.push(createPlayer());
+
+            while(data.enemies.length > $scope.enemies.length)
+                $scope.enemies.push(createEnemy());
+
+            while(data.fMissles.length > $scope.fMissles.length)
+                $scope.fMissles.push(createBody());
+
+            while(data.eMissles.length > $scope.eMissles.length)
+                $scope.eMissles.push(createEBody());
 
             //update movements
             var i = 0;
-            for(i = 0; i < data.eMissles.length; i++){
-                $scope.eMissles[i].style.left = data.eMissles[i].currentLeft + '%';
-                $scope.eMissles[i].style.top = data.eMissles[i].currentTop + '%';
-            }
-            for(i = 0; i < data.fMissles.length; i++){
-                $scope.fMissles[i].style.left = data.fMissles[i].currentLeft + '%';
-                $scope.fMissles[i].style.top = data.fMissles[i].currentTop + '%';
-            }
-            for(i = 0; i < data.enemies.length; i++){
-                $scope.enemies[i].style.left = data.enemies[i].currentLeft + '%';
-                $scope.enemies[i].style.top = data.enemies[i].currentTop + '%';
-            }
-            for(i = 0; i < data.players.length; i++){
-                $scope.players[i].style.left = data.players[i].currentLeft + '%';
-                $scope.players[i].style.top = data.players[i].currentTop + '%';
-                $scope.players[i].className = playerBaseClass + ' ' + data.players[i].class;
-            }
+            for(i = 0; i < data.eMissles.length; i++)
+                updatePos($scope.eMissles[i], data.eMissles[i]);
+
+            for(i = 0; i < data.fMissles.length; i++)
+                updatePos($scope.fMissles[i], data.fMissles[i]);
+
+            for(i = 0; i < data.enemies.length; i++)
+                updatePos($scope.enemies[i], data.enemies[i]);
+
+            for(i = 0; i < data.players.length; i++)
+                updatePos($scope.players[i], data.players[i]);
+
+            if($scope.player)
+                updatePos($scope.player, data.player);
         });
 
-        $scope.createBody = function(){
+        //Element removal functions
+        function removeFMissle(missle, index){
+            if(missle){
+                missle.parentNode.removeChild(missle);
+                $scope.fMissles.splice(index, 1);
+            }
+        }
+
+        function removeEMissles(missle, index){
+            if(missle){
+                missle.parentNode.removeChild(missle);
+                $scope.eMissles.splice(index, 1);
+            }
+        }
+
+        function removeEnemy(enemy, index){
+            if(enemy){
+                enemy.parentNode.removeChild(enemy);
+                $scope.enemies.splice(index, 1);
+            }
+        }
+
+        function removePlayer(player, index){
+            if(player){
+                player.parentNode.removeChild(player);
+                $scope.players.splice(index, 1);
+            }
+        }
+
+        //Element Position movement functions
+        function updatePos(element, data){
+            element.style.left = data.currentLeft + '%';
+            element.style.top = data.currentTop + '%';
+        }
+
+        //Element creation functions
+        var createBody = function(){
             var missle = document.createElement("i");
 
             missle.className = 'fa fa-ellipsis-h';
@@ -98,10 +130,10 @@ angular.module('plane-game').controller('planeGameMultiController', ['$rootScope
 
             field.append( missle);
 
-            $scope.fMissles.push(missle);
+            return missle;
         };
 
-        $scope.createEBody = function(){
+        var createEBody = function(){
             var missle = document.createElement("i");
 
             missle.className = 'fa fa-ellipsis-h text-danger';
@@ -111,10 +143,10 @@ angular.module('plane-game').controller('planeGameMultiController', ['$rootScope
 
             field.append( missle);
 
-            $scope.eMissles.push(missle);
+            return missle;
         };
 
-        $scope.createEnemy = function(top, left){
+        var createEnemy = function(top, left){
             var enemy = document.createElement("i");
 
             enemy.className = 'fa fa-fighter-jet fa-rotate-180 text-muted';
@@ -124,10 +156,10 @@ angular.module('plane-game').controller('planeGameMultiController', ['$rootScope
 
             field.append( enemy);
 
-            $scope.enemies.push(enemy);
+            return enemy;
         };
 
-        $scope.createPlayer = function(){
+        var createPlayer = function(){
             var player = document.createElement("i");
 
             player.className = playerBaseClass;
@@ -137,9 +169,10 @@ angular.module('plane-game').controller('planeGameMultiController', ['$rootScope
 
             field.append( player);
 
-            $scope.players.push(player);
+            return player;
         };
 
+        //Control handling
         document.onclick = function() {
             $scope.socket.emit('control-shoot', {});
         };
@@ -147,46 +180,50 @@ angular.module('plane-game').controller('planeGameMultiController', ['$rootScope
         document.onkeydown = function(e){
             var evt = e || window.event;
 
-            if(evt.keyCode === 40){
-                $scope.playerMv.mvD = true;
-                $scope.socket.emit('control-movement', $scope.playerMv);
-            }
-            if(evt.keyCode === 38){
-                $scope.playerMv.mvU = true;
-                $scope.socket.emit('control-movement', $scope.playerMv);
-            }
-            if(evt.keyCode === 37){
-                $scope.playerMv.mvR = true;
-                $scope.socket.emit('control-movement', $scope.playerMv);
-            }
-            if(evt.keyCode === 39){
-                $scope.playerMv.mvL = true;
-                $scope.socket.emit('control-movement', $scope.playerMv);
-            }
+            if(!evt.repeat){
+                if(evt.keyCode === 40 || evt.keyCode === 83){
+                    $scope.playerMv.mvD = true;
+                    $scope.socket.emit('control-movement', $scope.playerMv);
+                }
+                if(evt.keyCode === 38 || evt.keyCode === 87){
+                    $scope.playerMv.mvU = true;
+                    $scope.socket.emit('control-movement', $scope.playerMv);
+                }
+                if(evt.keyCode === 37 || evt.keyCode === 65){
+                    $scope.playerMv.mvR = true;
+                    $scope.socket.emit('control-movement', $scope.playerMv);
+                }
+                if(evt.keyCode === 39 || evt.keyCode === 68){
+                    $scope.playerMv.mvL = true;
+                    $scope.socket.emit('control-movement', $scope.playerMv);
+                }
 
-            if(evt.keyCode === 32 && !evt.repeat){
-                $scope.socket.emit('control-shoot', {});
+                if(evt.keyCode === 32){
+                    $scope.socket.emit('control-shoot', {});
+                }
             }
         };
 
         document.onkeyup = function(e){
             var evt = e || window.event;
 
-            if(evt.keyCode === 40){
-                $scope.playerMv.mvD = false;
-                $scope.socket.emit('control-movement', $scope.playerMv);
-            }
-            if(evt.keyCode === 38){
-                $scope.playerMv.mvU = false;
-                $scope.socket.emit('control-movement', $scope.playerMv);
-            }
-            if(evt.keyCode === 37){
-                $scope.playerMv.mvR = false;
-                $scope.socket.emit('control-movement', $scope.playerMv);
-            }
-            if(evt.keyCode === 39){
-                $scope.playerMv.mvL = false;
-                $scope.socket.emit('control-movement', $scope.playerMv);
+            if(!evt.repeat) {
+                if(evt.keyCode === 40 || evt.keyCode === 83){
+                    $scope.playerMv.mvD = false;
+                    $scope.socket.emit('control-movement', $scope.playerMv);
+                }
+                if(evt.keyCode === 38 || evt.keyCode === 87){
+                    $scope.playerMv.mvU = false;
+                    $scope.socket.emit('control-movement', $scope.playerMv);
+                }
+                if(evt.keyCode === 37 || evt.keyCode === 65){
+                    $scope.playerMv.mvR = false;
+                    $scope.socket.emit('control-movement', $scope.playerMv);
+                }
+                if(evt.keyCode === 39 || evt.keyCode === 68){
+                    $scope.playerMv.mvL = false;
+                    $scope.socket.emit('control-movement', $scope.playerMv);
+                }
             }
         };
     }
--- server/modules/plane-game/plane-game.server.sockets.js
+++ server/modules/plane-game/plane-game.server.sockets.js
@@ -22,11 +22,11 @@ var fMissles = [],
     playerMvVert = 5,
     playerMvHoz = 0.75,
     playerCnt = 0,
-    enemySpawnInterval;
+    enemySpawnInterval,
+    updatePlayerMv = {},
+    shoot = {};
 
 module.exports = function(io){
-    var messages = [];
-
     //Initialization and disconnection
     io.on('connection', function (client) {
         function indivualClients(client){
@@ -36,46 +36,52 @@ module.exports = function(io){
                 delete clients[client.handshake.address]; //no disconnect means no re-connect, new sockets only
 
                 if(multiClients[client.handshake.address]){
-                    playerCnt--;
-                    delete players[client.handshake.address]; //if player exist, remove it
-                    delete multiClients[client.handshake.address]; //no disconnect means no re-connect, new sockets only
+                    removePlayer(client.handshake.address);
+                    delete multiClients[client.handshake.address];
                 }
             });
 
             //Multiplier game
             client.on('join-multiplier', function (data) {
                 playerCnt++;
-                if(!multiClients[client.handshake.address]){
-                    multiClients[client.handshake.address] = client;
-                    var player = players[client.handshake.address] = {currentTop: 50, currentLeft: 10, mvL: false, mvR: false, mvU: false, mvD: false};
+                var id = client.handshake.address;
+
+                if(!multiClients[id]){
+                    multiClients[id] = client;
+
+                    var player = {
+                        currentTop: 50,
+                        currentLeft: 10,
+                        mvL: false,
+                        mvR: false,
+                        mvU: false,
+                        mvD: false,
+                        players: Object.keys(players).map(function(key){
+                            return players[key];
+                        })
+                    };
+
+                    for(var key in players)
+                        players[key].players.push(player);
+
+                    players[id] = player;
 
                     //Multiplier game
-                    client.on('control-movement', function (data) {
+                    updatePlayerMv[id] = function(data) {
                         player.mvL = data.mvL || false;
                         player.mvR = data.mvR || false;
                         player.mvU = data.mvU || false;
                         player.mvD = data.mvD || false;
-                    });
+                    };
 
                     //Multiplier game
-                    client.on('control-shoot', function (data) {
+                    shoot[id] = function() {
                         createFMissle(player);
-                    });
+                    };
 
-                    //Multiplier game
-                    client.on('start-game', function (data) {
-                        console.log('Attempting to Start Game');
-                        console.log(enemySpawnInterval);
-                        if(!enemySpawnInterval){
-                            console.log('Starting Game');
-
-                            enemySpawnInterval = setInterval(function(){
-                                var ran = getRandomArbitrary(1, 5);
-                                if((enemies.length <= 10) && (ran === 5))
-                                    createEnemy();
-                            }, 100);
-                        }
-                    });
+                    client.on('control-movement', updatePlayerMv[id]);
+                    client.on('control-shoot', shoot[id]);
+                    client.on('start-game', startGame);
                 }
             });
         }
@@ -88,33 +94,28 @@ module.exports = function(io){
     //This dictates the update and movement rate of all things in game
     var gameInterval = setInterval(function(){
         //If no players then reset the game
-        if(playerCnt === 0){
+        if(playerCnt === 0 && enemySpawnInterval)
             restartGame();
-        }
 
         var i = 0,
             key,
-            client;
+            player;
 
         //updates the movements of all enemies
-        for(i =0; i< enemies.length; i++){
+        for(i = 0; i < enemies.length; i++)
             updateEnemy(enemies[i], i);
-        }
 
         //updates the movements of all enemy bullets
-        for(i =0; i< eMissles.length; i++){
+        for(i = 0; i < eMissles.length; i++)
             updateEMissle(eMissles[i], i);
-        }
 
         //updates the movements of all friendly bullets
-        for(i =0; i< fMissles.length; i++){
+        for(i = 0; i < fMissles.length; i++)
             updateFMissle(fMissles[i], i);
-        }
 
         //updates all players movements
-        for(key in players){
+        for(key in players)
             updatePlayer(players[key], key);
-        }
 
         //This makes enemies shoot, random enemy at random time
         if(getRandomArbitrary(1, 7) === 5 && enemies.length)
@@ -122,11 +123,49 @@ module.exports = function(io){
 
         //Send all players an update of all positions of bullets, players and enemies
         for(key in multiClients){
-            client = multiClients[key];
-            client.emit('multiplier-update', {enemies: enemies, eMissles: eMissles, fMissles: fMissles, players: players, id: client.handshake.address});
+            player = players[key];
+            if(player)
+                multiClients[key].emit('multiplier-update', {
+                    enemies: enemies,
+                    eMissles: eMissles,
+                    fMissles: fMissles,
+                    players: Object.keys(player.players).map(function(key){
+                        return {
+                            currentTop: player.players[key].currentTop,
+                            currentLeft: player.players[key].currentLeft
+                        };
+                    }),
+                    player: {
+                        currentTop: player.currentTop,
+                        currentLeft: player.currentLeft
+                    }
+                });
         }
     }, gameRate);
 };
+
+function removePlayer(id){
+    playerCnt--;
+    delete players[id]; //if player exist, remove it
+
+    var client = multiClients[id];
+    client.removeListener('control-movement', updatePlayerMv[id]);
+    client.removeListener('control-shoot', shoot[id]);
+    client.removeListener('start-game', startGame);
+}
+
+//Multiplier game
+function startGame(){
+    console.log('Attempting to Start Game');
+    if(!enemySpawnInterval){
+        console.log('Starting Game');
+
+        enemySpawnInterval = setInterval(function(){
+            if((enemies.length <= 10) && ( getRandomArbitrary(1, 6) === 5))
+                createEnemy();
+        }, 100);
+    }
+}
 
 function createEnemy(){
     enemies.push({
@@ -163,30 +202,23 @@ function updateEnemy(enemy, index){
 
     var ran = getRandomArbitrary(0, 15);
 
-    if(ran === 3 || enemy.currentTop === 100){
+    if(ran === 3 || enemy.currentTop === 100)
         enemy.topDif -= getRandomArbitrary(1, 2);
-    }
 
-    if(ran === 2 || enemy.currentTop === 0){
+    if(ran === 2 || enemy.currentTop === 0)
         enemy.topDif += getRandomArbitrary(1, 2);
-    }
 
-    if(ran === 10){
-    }
-
+    //Shift up, down or not at all
     enemy.currentTop += enemy.topDif;
 
-    if(enemy.currentTop < 0){
+    if(enemy.currentTop < 0)
         enemy.currentTop = 0;
-    }
 
-    if(enemy.currentTop > 100){
+    if(enemy.currentTop > 100)
         enemy.currentTop = 100;
-    }
 
-    if((enemy.currentLeft < 0) || enemy.dead){
+    if((enemy.currentLeft < 0) || enemy.dead)
         enemies.splice(index, 1);
-    }
 }
 
 function updateEMissle(missle, index){
@@ -203,8 +235,7 @@ function updateEMissle(missle, index){
             if(( (player.currentLeft + window) > missle.currentLeft) && (player.currentLeft - window) < missle.currentLeft){
                 if(( (player.currentTop + window) > missle.currentTop) && (player.currentTop - window) < missle.currentTop){
                     multiClients[key].emit('you-dead', {});
-                    delete players[key];
-                    playerCnt--;
+                    removePlayer(key);
                 }
             }
         }
@@ -263,4 +294,26 @@ function restartGame(){
     enemies = [];
     fMissles = [];
     eMissles = [];
+
+    blankPlayersField();
+    multiClients = {};
+    console.log('restarting game');
+}
+
+function blankPlayersField(){
+    //Send all players an update of all positions of bullets, players and enemies
+    for(var key in multiClients) {
+        console.log('Blanked ' + key + '\'s field');
+
+        multiClients[key].emit('multiplier-update', {
+            enemies: [],
+            eMissles: [],
+            fMissles: [],
+            players: [],
+            player: {
+                currentTop: 0,
+                currentLeft: 0
+            }
+        });
+    }
 }
